c42628b574a388dca1c2ff40464aae53bd17b201
jack8096 <jackdeepakkamble@gmail.com>
2026-09-02 17:34:09 +0530

    save progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  final messaging = FirebaseMessaging.instance;
+  messaging.requestPermission(
+    alert: true,
+    announcement: true,
+    badge: true,
+    carPlay: true,
+    criticalAlert: true,
+    provisional: true,
+    sound: true,
+  );
+  messaging.getToken().then((token) {
+    print('fcm token: $token');
+  });
   runApp(const MyApp());
 }
 
