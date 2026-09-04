5df081c615c28446fd4f277a1a2ed9590e3e5f37
jack8096 <95523743+jack8096@users.noreply.github.com>
2026-09-04 21:31:20 +0530

    save progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,48 @@
+import 'package:firebase_app_installations/firebase_app_installations.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+final navigatorKey = GlobalKey<NavigatorState>();
+
+Widget createDialog(String title) {
+  return AlertDialog(
+    title: Text(title),
+    actions: [
+      ElevatedButton(
+        onPressed: () {
+          final ctx = navigatorKey.currentContext;
+          if (ctx == null) return;
+          Navigator.of(ctx).pop();
+        },
+        child: const Text('OK'),
+      ),
+    ],
+  );
+}
+
+Future<void> handleMsg(RemoteMessage? msg) async {
+  if (msg == null) return;
+  var ctx = navigatorKey.currentContext;
+  var dialog = createDialog(msg.notification?.title ?? "no title");
+  if (ctx == null || ctx.mounted == false) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (ctx == null) return;
+      showDialog(context: ctx, builder: (context) => dialog);
+    });
+  } else {
+    showDialog(context: ctx, builder: (context) => dialog);
+  }
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  var fDeviceID = await FirebaseInstallations.instance.getId();
+  debugPrint('firebase_app_installations id: $fDeviceID');
   final messaging = FirebaseMessaging.instance;
   messaging.requestPermission(
     alert: true,
@@ -16,55 +54,40 @@ void main() async {
     sound: true,
   );
   messaging.getToken().then((token) {
-    print('fcm token: $token');
+    debugPrint('fcm token: $token');
   });
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    FirebaseMessaging.instance.getInitialMessage().then(handleMsg);
+    FirebaseMessaging.onMessageOpenedApp.listen(handleMsg);
+    FirebaseMessaging.onMessage.listen(handleMsg);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -100,7 +123,6 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
